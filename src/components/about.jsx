d7c3505d1021102892aeaf2cd761c3f7e36cc019
juliaveronica02 <juliavero2206@gmail.com>
2021-02-23 08:35:46 +0700

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class about extends Component {
+  render() {
+    return (
+        <div id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-md-6"> <img src="img/about.jpg" className="img-responsive" alt=""/> </div>
+            <div className="col-xs-12 col-md-6">
+              <div className="about-text">
+                <h2>About Us</h2>
+                <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
+                <h3>Why Choose Us?</h3>
+                <div className="list-style">
+                  <div className="col-lg col-sm col-xs">
+                    <ul>
+                      {this.props.data ? this.props.data.Why.map((d, i) => <li  key={`${d}-${i}`}>{d}</li>) : 'loading'}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default about
